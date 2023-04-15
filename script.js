1587b2065c4fea66f2e8fdfb378017df7846b71b
Joao Vitor Siqueira <jvitorsiqueira99@gmail.com>
2023-04-15 17:43:16 -0300

    Added timer and score display at the end

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const board = document.getElementById("textContainer");
 const timer = document.getElementById("timer");
 let wordsTyped = 0;
 let misses = 0;
+let secondsLeft = 60;
 
 let regExp = /\w+/g;
 let allWords = [...board.innerHTML.matchAll(regExp)];
@@ -11,10 +12,21 @@ let spansLength = 0;
 
 
 
+function countDown(){
+    let myinterval = setInterval(() =>{
+        timer.innerHTML = secondsLeft.toString();
+        secondsLeft --
+        if(secondsLeft < 0){
+            clearInterval(myinterval)
+        }
+    },1000)
+}
 
-//metaphorpsum.com/paragraphs/<numberOfParagraphs>/<numberOfSentences>
+
+
 
 function checkWord(e){
+    
     let currentWord = allWords[tries][0];
     
     if(e.value.length >= currentWord.length){
@@ -31,15 +43,33 @@ function checkWord(e){
         board.scrollTop += 5;
         
     }
-    if(tries == allWords.length){
-        console.log(`Words typed:${wordsTyped}\nMisses:${misses}`);
+    if(tries == allWords.length || secondsLeft == 0){
+        removeChilds(document.body)
+        showResults();
     }
     
-    console.log(allWords)
+   
 }
 
 
+function showResults(){
+    removeChilds(document.body)
+    let div = document.createElement('div');
+    if(secondsLeft > 0){
+        wordsTyped = Math.floor((wordsTyped * 60) / (60 - secondsLeft)); 
+    }
 
+    div.classList.add("results");
+    div.innerHTML = `Words per minute: ${wordsTyped}<br>Misses: ${misses}`; 
+    document.body.appendChild(div);   
+    
+}
+
+function removeChilds(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 String.prototype.replaceWord = function(index, replacement,word) {
     return this.substring(0, index) + replacement + this.substring(index + word.length);
